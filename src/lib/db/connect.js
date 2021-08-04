0167eb12060a8f255cac7eb2fa2406c7f36e6601
smj-edison <stephen.mason.jones@gmail.com>
2021-08-04 16:03:21 -0700

    Add file to connect to DB

---
+++ src/lib/db/connect.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+import neo4j from 'neo4j-driver';
+
+dotenv.config();
+
+const driver = new neo4j.driver(process.env.DB_URI, neo4j.auth.basic(process.env.DB_USERNAME, process.env.DB_PASSWORD));
+
+export function getSession() {
+    return driver.session();
+}
